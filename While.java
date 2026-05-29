2432e116db8a1eb80b564aaf2a7bcff1b72f1365
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-05-29 13:05:13 +0530

    added loop

---
+++ While.java
@@ -0,0 +1,15 @@
+
+import java.util.*; 
+public class While {
+    public static void main(String[] args) {
+        
+    
+    int count = 1;
+    while(count !=5){
+        System.out.print(count + " ");
+       count++;
+    }
+    
+    
+}
+}
