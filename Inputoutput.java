e135753bed089cd82802a81ed01f007555085847
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-05-29 13:00:06 +0530

    added my first java inputout code

---
+++ Inputoutput.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class Inputoutput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("please unter your age:");
+        int age = input .nextInt();
+        System.out.println("your age is:" +age);
+        
+       
+    }
+}
