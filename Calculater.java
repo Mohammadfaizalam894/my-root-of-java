1fb11efcf09ceab4a7edeb75f95b88456ce059a3
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-05-30 09:35:35 +0530

    calculater programme

---
+++ Calculater.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Calculater {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        //taking the input
+        int ans = 0;
+        while(true){
+            System.out.println("enter the operator:");
+            char op = sc.next().trim().charAt(0);
+            if(op =='+' || op =='-' || op =='*' || op =='/' || op =='%' ){
+                //input two numbers
+
+                System.out.println("enter the two numbers:");
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+                if (op == '+'){
+                    ans = num1 +num2;
+                }
+                if (op == '-'){
+                    ans = num1 -num2;
+                }
+                if (op == '*'){
+                    ans = num1 *num2;
+                }
+                if (op == '%'){
+                    ans = num1 % num2;
+                }
+                if (op == '/'){
+                    ans = num1 / num2;
+                }
+                
+        }
+         else if (op=='x' ||op == 'X')
+            {
+            break;
+            }
+        else
+            {
+            System.out.println("invalid operator");
+        
+            }
+            System.out.println(ans);
+
+    }
+    
+}
+}
