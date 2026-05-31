906e6b45b5037a684d4d21474b2479f480b0dba4
Mohammad Faiz Alam <mohdfaizalam894@gmail.com>
2026-05-31 11:45:00 +0530

    added new code

---
+++ LinearSearchString.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LinearSearchString {
+    public static void main(String[] args){
+        String ch[] = {"faiz","alam", "jack","devid","peter"};
+        
+
+    }
+    //
+}
